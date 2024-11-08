3213543d106d9d986bcaa1d53967ec952bb003c9
Gui3150 <127741744+Gui3150@users.noreply.github.com>
2024-11-08 11:15:10 -0300

    Add files via upload

---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Dicas de Código</title>
+    <link rel="stylesheet" href="/css/estilo.css">
+</head>
+<body>
+    <h3>Sobre Dicas de Código</h3>
+    <h4> O que é Laravel? </h4>
+
+
+
+<p> 
+O Laravel é um dos frameworks PHP mais utilizados no mercado. Assim como outros do tipo, como Symfony e CodeIgniter, um framework é um conjunto de ferramentas, recursos e funcionalidades, criado em uma determinada linguagem de programação para facilitar e agilizar tarefas comuns de desenvolvimento de sistemas, como autenticação, localização, sessões e cache.
+</p>
+
+<p>
+Criado pelo programador Tyler B. Otwell como uma alternativa mais avançada ao CodeIgniter, teve sua primeira versão lançada em 2011. Desde então, tem recebido atualizações e contribuições constantes tanto do autor quanto da comunidade, posicionando-se como um verdadeiro ecossistema para criação de sistemas para web. Com foco na sintaxe simples e concisa, seu mote é “O framework PHP dos artesãos da web”.
+</p> 
+
+<h4> Principais recursos</h4>
+
+<p>
+O Laravel é baseado na arquitetura MVC (acrônimo para Model-View-Controller, ou Modelo-Visão-Controle, em português). MVC é um padrão de arquitetura de software focado em reuso de código, no qual ocorre a divisão da estrutura lógica de um sistema em 3 camadas: a do Modelo, relacionada ao banco de dados; a de Visão, vinculada a visualização dos dados e das páginas; e a do Controle, responsável pela conexão e transmissão de informações entre as camadas Modelo e Visão.
+</p>
+
+<p>
+Além disso, o Laravel tem como principais recursos a utilização de um sistema modular para gerenciamento de dependências, diferentes formas de conexão e acesso a banco de dados relacionais, um “motor” próprio de templates para criação de interfaces, além de vários programas e serviços criados para facilitar a publicação e manutenção de sistemas criados com o framework.
+</p>
+
+<h4> Versões e Suporte </h4>
+<p>
+A versão mais recente é o Laravel 8, lançado em setembro de 2020. Versões nomeadas com a sigla LTS recebem suporte estendido para resolução de bugs por 2 anos e de problemas de segurança por 3 anos, como o Laravel 5.1 LTS, 5.5 LTS e 6 LTS. 
+</p>
+</body>
+</html>
